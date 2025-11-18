ecabad78246ba18d6535ecb83b6730e9f0c47785
meera <meeghelani@gmail.com>
2025-11-18 21:28:49 +0530

    Update auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -22,4 +22,28 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        });
+        
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+        
+        res.json({ message: 'Login successful', 
+            user: data.user,
+            session: data.session,
+            accessToken: data.session.access_token
+         });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
 module.exports = router;
